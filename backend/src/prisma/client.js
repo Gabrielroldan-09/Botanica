56c82dab55c0f4110744c70338bd3b4283a3be46
Gabrielroldan-09 <gabyroldan77uhshs@gmail.com>
2026-04-17 20:05:43 -0300

    Update client.js

--- backend/src/prisma/client.js
+++ backend/src/prisma/client.js
@@ -1,10 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-  log: process.env.NODE_ENV === 'development'
-    ? ['query', 'error', 'warn']
-    : ['error'],
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
